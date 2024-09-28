26d01cb065bd7b9686c93e861edd5d0980bd1bb7
venilson2 <venilson2009@hotmail.com>
2024-09-27 22:18:19 -0300

    implementing and testing other methods

--- application/product_test.go
+++ application/product_test.go
@@ -13,3 +13,26 @@ func TestProduct_Enable(t *testing.T) {
 	err = product.Enable()
 	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
 }
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
+}
